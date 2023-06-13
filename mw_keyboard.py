d39612b0088eefd684eb7c08cb19a52cfe9e6957
froggyriia <nira.osamu@yandex.ru>
2023-06-13 14:14:11 +0300

    first commit

---
+++ mw_keyboard.py
@@ -0,0 +1,29 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from aiogram.utils.callback_data import CallbackData
+
+show_schedule = InlineKeyboardButton('Посмотреть расписание🙊🗓', callback_data='show_schedule')
+show_homework = InlineKeyboardButton('Посмотреть домашнее задние 📖📚', callback_data='show_homework')
+edit_schedule = InlineKeyboardButton('Редактировать расписание ✏️🗓', callback_data='edit_schedule')
+edit_homework = InlineKeyboardButton('Редактировать домашнее задние ✏️📖', callback_data='edit_homework')
+show_a_cat_kb = InlineKeyboardButton('Получить дозу мотивации🌷🐱🌷', callback_data='show_a_cat')
+
+inlineKeyboardGreeting = InlineKeyboardMarkup(row_width=1).add(show_schedule,
+                                                               show_homework,
+                                                               edit_homework,
+                                                               edit_schedule,
+                                                               show_a_cat_kb)
+"""Клавиатура-меню, чтобы `пользователь такой *тык* и его перекидывало на нужные штуки`"""
+
+weekday_cd = CallbackData("w_w", "weekday")
+monday = InlineKeyboardButton('Понедельник', callback_data=weekday_cd.new(weekday='monday'))
+tuesday = InlineKeyboardButton('Вторник', callback_data=weekday_cd.new(weekday='tuesday'))
+wednesday = InlineKeyboardButton('Среда', callback_data=weekday_cd.new(weekday='wednesday'))
+thursday = InlineKeyboardButton('Четверг', callback_data=weekday_cd.new(weekday='thursday'))
+friday = InlineKeyboardButton('Пятница', callback_data=weekday_cd.new(weekday='friday'))
+saturday = InlineKeyboardButton('Суббота', callback_data=weekday_cd.new(weekday='saturday'))
+sunday = InlineKeyboardButton('Воскресенье', callback_data=weekday_cd.new(weekday='sunday'))
+week_schedule = [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
+
+inlineKeyboardWeekSchedule = InlineKeyboardMarkup().add(*week_schedule)
+"""Клавиатура содержащая дни недели"""
---
+++ project_bot_hw.py
@@ -0,0 +1,282 @@
+from __future__ import annotations
+
+import io
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+from env import MV_TOKEN
+import logging
+import requests
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.types import Message, InputFile
+from mw_keyboard import inlineKeyboardGreeting, inlineKeyboardWeekSchedule, weekday_cd
+from enum import Enum
+
+# Включает подробное логгирования для бота (в консоль)
+logging.basicConfig(level=logging.INFO)
+
+
+class Day(Enum):
+    """Класс для дней недели"""
+    MONDAY = "monday"
+    TUESDAY = "tuesday"
+    WEDNESDAY = "wednesday"
+    THURSDAY = "thursday"
+    FRIDAY = "friday"
+    SATURDAY = "saturday"
+    SUNDAY = "sunday"
+
+
+class BotStates(StatesGroup):
+    """Хранит все возможные стейты пользователя в Finite State Machine"""
+    edit_schedule_for_weekday = State()
+
+    choose_weekday_to_edit_schedule = State()
+    choose_weekday_to_show_schedule = State()
+
+    choose_weekday_to_edit_homework = State()
+    choose_weekday_to_show_homework = State()
+
+
+class Homework:
+    owner_id: int
+
+    homework_by_days: dict[Day, str]
+
+    __instances__: dict[int, Homework] = {}
+
+    def __init__(self, owner_id: int):
+        self.homework_by_days = dict()
+        self.owner_id = owner_id
+
+    @staticmethod
+    def get_or_create_homework(owner_id: int):
+        if owner_id not in Homework.__instances__:
+            Homework.__instances__[owner_id] = Homework(owner_id)
+        return Homework.__instances__[owner_id]
+
+    @staticmethod
+    def get(owner_id: int):
+        return Homework.__instances__.get(owner_id)
+
+    def get_for_day(self, day: Day | str):
+        day = Day(day)
+        return self.homework_by_days.get(day)
+
+    def set_for_day(self, day: Day | str, assignments: str):
+        day = Day(day)
+        self.homework_by_days[day] = assignments
+
+    def __str__(self):
+        return str(self.homework_by_days)
+
+class Schedule:
+    """Класс для расписания (для отдельного пользователя), хранит все расписания по ключу (id пользователя)"""
+    owner_id: int
+    schedule_by_days: dict[Day, str]
+    """Расписание отдельного пользователя"""
+    __instances__: dict[int, Schedule] = {}
+    """Нужно для хранение расписаний пользователей"""
+
+    def __init__(self, owner_id: int):
+        self.schedule_by_days = dict()
+        self.owner_id = owner_id
+
+    @staticmethod
+    def get_or_create_schedule(owner_id: int) -> Schedule:  # Singleton or FlyWeight
+        """Создаёт либо выдаёт уже существующее расписание по ключу(id пользователя)
+
+        :param owner_id: id пользователя
+        :return: возвращает либо новое расписание, либо то, что уже существует
+        """
+        if owner_id not in Schedule.__instances__:
+            Schedule.__instances__[owner_id] = Schedule(owner_id)
+        return Schedule.__instances__[owner_id]
+
+
+    @staticmethod
+    def get(owner_id: int) -> Schedule | None:
+        """Выдаёт уже существующее расписание по ключу(id пользователя), либо возвращает None
+
+        :param owner_id: id пользователя
+        :return: возвращает существующее расписание либо None
+        """
+        return Schedule.__instances__.get(owner_id)
+
+    def get_for_day(self, day: Day | str):
+        """Возвращает расписание на день
+
+        :param day: день недели
+        :return: список предметов (пока что строка)
+        """
+        day = Day(day)
+        return self.schedule_by_days.get(day)
+
+    def set_for_day(self, day: Day | str, subjects: str):
+        """Добавляет предметы в расписание пользователя на день
+
+        :param day: день недели
+        :param subjects: список/строка предметов
+        """
+        day = Day(day)
+        self.schedule_by_days[day] = subjects
+
+    def __str__(self):
+        return str(self.schedule_by_days)
+
+
+# Инициализировать бота и задать диспетчера
+bot = Bot(token=MV_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'], state="*")
+async def start(message: Message, state: FSMContext):
+    """Обработчик для запуска бота:
+    1) Получаем расписание пользователя если оно есть, иначе — создаём.
+    2) Отправляем привественное сообщение пользователю с клавиатурой
+    """
+    schedule = Schedule.get_or_create_schedule(message.from_id)
+    await message.answer(
+        f'Hi, {message.from_user.username}! Welcome to Motivation and Study Bot!'
+        '\nYou can set your schedule and homework here'
+        '\n(or get a motivation pic ;)'
+        '\nUse buttons below to choose the mode'
+        '\nStay motivated and do your homework <3', reply_markup=inlineKeyboardGreeting)
+
+
+@dp.callback_query_handler(text='show_schedule', state="*")
+async def show_schedule(call: types.CallbackQuery, state: FSMContext):
+    """callback для кнопки "посмотреть расписание":
+     1) Выводится лист кнопок - список дней недели
+     2) Устанавливается state для выбора дня недели для демонстрации расписания"""
+    await bot.send_message(call.from_user.id, 'Вот твое расписание: ', reply_markup=inlineKeyboardWeekSchedule)
+    await BotStates.choose_weekday_to_show_schedule.set()
+
+
+#
+@dp.callback_query_handler(weekday_cd.filter(), state=BotStates.choose_weekday_to_show_schedule)
+async def _(call: types.CallbackQuery, callback_data: dict, state: FSMContext):
+    """callback для кнопки дней недели (для демонстрации расписания)
+    1) Получаем значение weekday - день недели, кнопка которго была активирована
+    2) Получаем расписание пользователя (оно точно существует см. start)
+    3) Из расписания пользователя получаем расписание на день
+    4) Проверяем не пустое ли расписание на день.
+    Если пустое - просим пользователя добавить расписание, иначе выводим расписание на день"""
+    weekday = callback_data.get("weekday")
+    schedule: Schedule = Schedule.get(call.from_user.id)
+    day_schedule = schedule.get_for_day(weekday)
+    if day_schedule is None:
+        await call.message.answer("Сначала добавь своё расписание")
+    else:
+        text = f"Вот твое расписание на день:\n{day_schedule}"
+        await call.message.answer(text)
+
+
+@dp.callback_query_handler(text='show_homework', state='*')
+async def show_homework(callback_query: types.CallbackQuery, state: FSMContext):
+    """callback для кнопки "посмотреть дз" - выводится лист кнопок - список дней недели
+    пока еще ничего нет"""
+    await bot.send_message(callback_query.from_user.id, 'Вот твое домашнее задание: ',
+                           reply_markup=inlineKeyboardWeekSchedule)
+    await BotStates.choose_weekday_to_show_homework.set()
+
+
+@dp.callback_query_handler(weekday_cd.filter(), state=BotStates.choose_weekday_to_show_homework)
+async def _(call: types.CallbackQuery, callback_data: dict):
+    """callback для кнопки дней недели (демонстрация домашнего задания)
+    пока еще ничего нет"""
+    weekday = callback_data.get("weekday")
+    print(weekday, 'just showing hw')
+
+
+@dp.callback_query_handler(text='edit_homework', state="*")
+async def edit_homework(callback_query: types.CallbackQuery, state: FSMContext):
+    """callback для кнопки "редактировать домашнее задание"
+     пока еще ничего нет"""
+    await bot.send_message(callback_query.from_user.id, 'Выбери день, для которго хочешь изменить домашнее задание: ',
+                           reply_markup=inlineKeyboardWeekSchedule)
+    await BotStates.choose_weekday_to_edit_homework.set()
+
+
+# callback для кнопки дней недели (изменение домашнего задания)
+@dp.callback_query_handler(weekday_cd.filter(), state=BotStates.choose_weekday_to_edit_homework)
+async def _(call: types.CallbackQuery, callback_data: dict):
+    weekday = callback_data.get("weekday")
+    print(weekday, 'ready to change the schedule hw')
+
+
+@dp.callback_query_handler(text='edit_schedule', state="*")
+async def edit_schedule(callback_query: types.CallbackQuery, state: FSMContext):
+    """callback для кнопки "редактировать расписание"
+        1) Просим пользователя выбрать день, для которого нужно изменить расписание,
+        и выводим клавиатуру - список дней недели
+        2) Устанавливается state для выбора дня, для которого нужно изменить расписание"""
+    # await bot.answer_callback_query(callback_query.id, 'Callback Answered!')  # это всплывающее окно
+    await bot.send_message(callback_query.from_user.id, 'Выбери день, который хочешь изменить: ',
+                           reply_markup=inlineKeyboardWeekSchedule)
+    await BotStates.choose_weekday_to_edit_schedule.set()
+
+
+@dp.callback_query_handler(weekday_cd.filter(), state=BotStates.choose_weekday_to_edit_schedule)
+async def _(call: types.CallbackQuery, callback_data: dict, state: FSMContext):
+    """callback для кнопки дней недели (изменение расписания)
+    1) Получаем weekday - день недели, кнопка которого была активирована. С помощью класса Day делаем его ТОЧНО днём
+    2) Запоминаем значение weekday
+    3) Установаливаем state для редактирования расписания определенного дня недели
+    4) Просим пользователя ввести список предметов"""
+    weekday = callback_data.get("weekday")
+    await state.update_data(chosen_weekday=weekday)
+    await BotStates.edit_schedule_for_weekday.set()
+    await call.message.answer('Введи свои предметы списком. Вот так:\nclass 1\nclass 2\nclass 3\n... ')
+
+
+@dp.message_handler(state=BotStates.edit_schedule_for_weekday)
+async def _(message: Message, state: FSMContext):
+    """Метод, который получает сообщение от пользователя и запоминает его список придметов в словарь schedule.day_schedule
+    по ключу [weekday]
+    1) Получаем значение weekday из state
+    2) Получаем расписание пользователя через его id
+    3) Из сообщения пользователя получаем список предметов, которые затем добавляем в его расписание
+    4) Отправляем расписание на день пользователю"""
+    data = await state.get_data()
+    weekday = data['chosen_weekday']
+
+    schedule = Schedule.get(message.from_id)
+
+    subjects = message.text
+    schedule.set_for_day(weekday, subjects)
+    print(schedule)
+
+    await message.answer(f'Твоё расписание\n: {schedule.schedule_by_days[weekday]}')
+
+
+# вот эта часть работает охуенно ее не трогать
+# callback для котиков
+@dp.callback_query_handler(text='show_a_cat', state="*")
+async def show_a_cat(callback_query: types.CallbackQuery):
+    img = get_cat()
+    if img:
+        # надо еще добавить всякие милости для подписей к картинкам
+        await bot.send_photo(callback_query.from_user.id, img, caption='котик', reply_markup=inlineKeyboardGreeting)
+
+
+def get_cat() -> InputFile:
+    link = 'https://api.thecatapi.com/v1/images/search'
+    answer: list[dict: str | int] = requests.get(link).json()
+    url_s = []
+    for i in answer:
+        url_s.append(i.get('url'))
+
+    for url in url_s:
+        data = requests.get(url).content
+        buf = io.BytesIO(data)
+        # buf.seek(0)
+        img = InputFile(buf)
+        return img
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
